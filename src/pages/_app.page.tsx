1f720cee87719fe367c5cfb0f2c38a294f66f53d
SnO2WMaN <me@sno2wman.net>
2021-09-14 23:58:32 +0900

    fix fontawsoem config

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,13 +3,17 @@ import {AppProps} from 'next/app';
 import {ApolloProvider} from '@apollo/client';
 import {detectLocale} from 'typesafe-i18n/detectors';
 import {SessionProvider} from 'next-auth/react';
+import {config as FontAwesomeConfig} from '@fortawesome/fontawesome-svg-core';
 
 import {createApolloClient} from '~/apollo/client';
 import {DefaultLayout} from '~/components/Layout';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import {Locales} from '~/i18n/i18n-types';
 import TypesafeI18n from '~/i18n/i18n-react';
 
 import '~/styles/index.css';
+
+FontAwesomeConfig.autoAddCss = false;
 
 // eslint-disable-next-line no-process-env
 if (process.env.NEXT_PUBLIC_API_MOCKING_ENABLED) require('../mocks');
